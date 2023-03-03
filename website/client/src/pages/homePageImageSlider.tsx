50873d0791ca29d9faf9f6d8ad8536cd26c81d42
rmansuri <mansurirushda7@gmail.com>
2023-03-03 00:38:45 -0500

    Homepage Image slider

---
+++ website/client/src/pages/homePageImageSlider.tsx
@@ -0,0 +1,70 @@
+import Carousel from 'react-multi-carousel'
+import { ResponsiveType } from 'react-multi-carousel/lib/types'
+import 'react-multi-carousel/lib/styles.css'
+import Image from 'next/image'
+
+const BreakpointSlides: ResponsiveType = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 530 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 530, min: 0 },
+    items: 1,
+  },
+}
+
+export default function Page() {
+  return (
+    <Carousel
+      responsive={BreakpointSlides}
+      ssr
+      infinite
+      itemClass="carousel-item"
+      autoPlay
+    >
+      <Image
+        alt=""
+        src="/images/ChildCare1.jpeg"
+        priority
+        layout="responsive"
+        width={700}
+        height={475}
+      />
+      <Image
+        alt=""
+        src="/images/SeniorCare1.jpeg"
+        priority
+        layout="responsive"
+        width={700}
+        height={475}
+      />
+      <Image
+        alt=""
+        src="/images/HouseCleaning1.jpeg" 
+        priority
+        layout="responsive"
+        width={700}
+        height={475}
+      />
+      <Image
+        alt=""
+        src="/images/PetCare1.jpeg"
+        layout="responsive"
+        width={700}
+        height={475}
+      />
+      <Image
+        alt=""
+        src="/images/Laundry1.jpeg"
+        layout="responsive"
+        width={700}
+        height={475}
+      />
+    </Carousel>
+  )
+}
